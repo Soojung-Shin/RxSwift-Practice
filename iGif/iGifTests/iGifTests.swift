71536d308634852afd8ad3030254635ac688893d
Soojung-Shin <ssj15987@naver.com>
2020-03-12 22:54:46 +0900

    들여쓰기 정리, 외부 라이브러리 변경사항 적용

--- iGif/iGifTests/iGifTests.swift
+++ iGif/iGifTests/iGifTests.swift
@@ -36,34 +36,34 @@ import OHHTTPStubs
 @testable import iGif
 
 class iGifTests: XCTestCase {
-  let obj = ["array": ["foo", "bar"], "foo": "bar"] as [String: AnyHashable]
-  let request = URLRequest(url: URL(string: "http://raywenderlich.com")!)
-  let errorRequest = URLRequest(url: URL(string: "http://rw.com")!)
-  
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    stub(condition: isHost("raywenderlich.com")) { _ in
-      return OHHTTPStubsResponse(jsonObject: self.obj, statusCode: 200, headers: nil)
+    let obj = ["array": ["foo", "bar"], "foo": "bar"] as [String: AnyHashable]
+    let request = URLRequest(url: URL(string: "http://raywenderlich.com")!)
+    let errorRequest = URLRequest(url: URL(string: "http://rw.com")!)
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        stub(condition: isHost("raywenderlich.com")) { _ in
+            return HTTPStubsResponse(jsonObject: self.obj, statusCode: 200, headers: nil)
+        }
+        stub(condition: isHost("rw.com")) { _ in
+            return HTTPStubsResponse(error: RxURLSessionError.unknown)
+        }
     }
-    stub(condition: isHost("rw.com")) { _ in
-      return OHHTTPStubsResponse(error: RxURLSessionError.unknown)
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        HTTPStubs.removeAllStubs()
     }
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-    OHHTTPStubs.removeAllStubs()
-  }
 }
 
 extension BlockingObservable {
-  func firstOrNil() -> E? {
-    do {
-      return try first()
-    } catch {
-      return nil
+    func firstOrNil() -> Element? {
+        do {
+            return try first()
+        } catch {
+            return nil
+        }
     }
-  }
 }
